839ae866e5b8e9abfb8ebe81272008dd26d239c1
Oliver <ovaningen@student.unimelb.edu.au>
2021-09-18 20:09:53 +1000

    Rudimentary permission system. Only admin users can create or delete organisations and departments. No other functionality is affected.

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import include
 
 from app.views import index, departments, organisations, customers, employees
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework import routers
+
 
 router = routers.SimpleRouter()
 router.register(r'departments', departments.DepartmentViewSet)
@@ -16,5 +18,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(r'accounts/', include('rest_auth.urls'))
 ]
 urlpatterns += router.urls
