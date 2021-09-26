36512517c8215fab731a2597ce47777dd48c71f5
Oliver <ovaningen@student.unimelb.edu.au>
2021-09-26 20:06:04 +1000

    Importing departments functionality

--- backend/app/views/departments.py
+++ backend/app/views/departments.py
@@ -1,11 +1,11 @@
 from django.http.response import HttpResponse
 from app.permissions import GetOnlyIfNotAdmin
-from rest_framework import viewsets
+from rest_framework import fields, viewsets
 from app.serializers import DepartmentSerializer
 from app.models import Department
 from rest_framework.decorators import action
 from import_export import resources
-
+import tablib
 
 # Department endpoints 
 
@@ -19,10 +19,17 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     ordering_fields = '__all__'
 
     @action(detail = False, methods=["GET"])
-    def export(self, request):
-        print(request.GET)
+    def export_data(self, request):
         queryset = self.get_queryset()
         filter_queryset = self.filter_queryset(queryset)
         resource = resources.modelresource_factory(model=Department)()
         data = resource.export(filter_queryset)
         return HttpResponse(data.json)
+
+    @action(detail = False, methods=["POST"])
+    def import_data(self, request):
+        resource = resources.modelresource_factory(model=Department)()
+        dataset = tablib.Dataset()
+        dataset.load(request.body, format="json")
+        result = resource.import_data(dataset, dry_run=False, raise_errors=True)
+        return HttpResponse(result.total_rows)
