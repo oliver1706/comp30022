b99782996737626e36084826ab0a466d8268d89a
yikyanc <yikyanc@student.unimelb.edu.au>
2021-10-09 00:09:53 +1100

    able to create plot for sales in base64

--- backend/app/views/invoices.py
+++ backend/app/views/invoices.py
@@ -5,10 +5,11 @@ from app.serializers import InvoiceSerializer
 from app.models import Invoice
 from app.permissions import GetOnlyIfNotAdmin 
 
-from django.db.models import Avg, Min, Max
+from django.db.models import Avg, Min, Max, Sum
 from rest_framework.response import Response
-
 from rest_framework.decorators import action
+from .utils import get_plot
+
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
@@ -19,7 +20,18 @@ class InvoiceViewSet(viewsets.ModelViewSet):
     ordering_fields = '__all__'
 
     @action(detail=False, methods = ['get'])
-    def stats(self, request, pk=None):
-        
+    def stats(self, request):
+        #this function return some basic statistics(mean,min,max) of all the invoices 
         stats=Invoice.objects.all().aggregate(mean=Avg('total_due'),min=Min('total_due'),max= Max('total_due'))
         return Response(stats)
+
+    @action(detail=False, methods = ['get'])
+    def salesplot(self,request):
+        #this function return the plot in base64 
+        # to view in html, use <img src ="data:image/png;base64, {{chart|safe}}"
+        sales_sum= Invoice.objects.all().values('date_added').annotate(sum = Sum('total_due'))
+        x=[x['date_added'] for x in sales_sum]
+        y=[y['sum'] for y in sales_sum]
+        chart=get_plot(x,y)
+        return Response({'chart':chart})
+  
--- backend/app/views/utils.py
+++ backend/app/views/utils.py
@@ -1,4 +1,7 @@
 from app.models import Department, Organisation
+from matplotlib import pyplot as plt
+import base64
+from io import BytesIO
 
 # If the validated_data contains a department id or a null department, update it
 def update_department_id(instance, validated_data):
@@ -16,3 +19,23 @@ def update_organisation_id(instance, validated_data):
         instance.organisation = organisation
     elif "organisation" in validated_data:
         instance.organisation = None
+
+def get_graph():
+    buffer=BytesIO()
+    plt.savefig(buffer,format='png')
+    buffer.seek(0)
+    image_png=buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph=graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_plot(x,y):
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(10,5))
+    plt.title('Sales by Date')
+    plt.plot(x,y)
+    plt.xlabel('Date')
+    plt.ylabel('Sum of Sales')
+    graph=get_graph()
+    return graph
